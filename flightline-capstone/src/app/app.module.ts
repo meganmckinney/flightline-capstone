3540be8268bfbfc8ce2e018295ede1b3cbf5d284
Megan McKinney <meganalexismckinney@gmail.com>
2021-09-13 14:29:30 -0500

    Created main component

--- flightline-capstone/src/app/app.module.ts
+++ flightline-capstone/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
