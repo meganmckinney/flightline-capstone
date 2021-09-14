023edecf76608e250728ef8da309e02a40230325
Megan McKinney <meganalexismckinney@gmail.com>
2021-09-14 01:17:28 -0500

    Added primeng, theme, and logo to header

--- flightline-capstone/src/app/app.module.ts
+++ flightline-capstone/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MenubarModule } from 'primeng/menubar'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { SharedModule } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { MainComponent } from './main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- flightline-capstone/src/app/header/header.component.ts
+++ flightline-capstone/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MenuItem } from 'primeng/api'
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,35 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  items: MenuItem[] = [];
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'File',
+              items: [{
+                      label: 'New',
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {label: 'Project'},
+                          {label: 'Other'},
+                      ]
+                  },
+                  {label: 'Open'},
+                  {label: 'Quit'}
+              ]
+          },
+          {
+              label: 'Edit',
+              icon: 'pi pi-fw pi-pencil',
+              items: [
+                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+              ]
+          }
+      ];
   }
 
 }
